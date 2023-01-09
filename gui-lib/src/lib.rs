1e5ba79b7f675ccf5f8f08e4512f57684f5399c2
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-01-09 17:31:44 -0600

    blue window

--- gui-lib/src/lib.rs
+++ gui-lib/src/lib.rs
@@ -90,11 +90,39 @@ impl GraphicsState {
     //     todo!()
     // }
 
-    fn update(&mut self) {
-        todo!()
+    pub fn update(&mut self) {
     }
 
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+    
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+    
+        Ok(())
     }
 }
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -48,8 +48,29 @@ pub async fn run() {
                 };
                 state.resize(new_size);
             }
+            
             _ => {}
         },
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                // Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // // The system is out of memory, we should probably quit
+                // Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    *control_flow = ControlFlow::Exit
+                },
+            }
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            window.request_redraw();
+        }
         _ => {}
     });
 }
